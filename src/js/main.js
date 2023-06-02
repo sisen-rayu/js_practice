31c4885d30de9f994ec49e882bc634fe8e684e0e
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2023-06-02 20:19:11 +0900

    Wip 5made

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ const $countBox = document.querySelector('#count');
 let count = 0;
 $countBox.innerHTML = count;
 
-setInterval(() => {
-    count++;
+const timer = setInterval(() => {
+    if(count >= 5) {
+        count = 0;
+    } else {
+      count++;
+    }
     $countBox.innerHTML = count;
 }, 1000);
