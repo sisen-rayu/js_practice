ab06456876c3f965e6553c31bbf5fce7f9242661
chihiro.tanigawa <chihiro.tanigawa@accenture.com>
2023-06-06 19:34:07 +0900

    JS build 問8

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,31 @@
+import '../scss/style.scss';
+
 const $countBox = document.querySelector('#count');
+const $buttonPlus = document.querySelector('.button_plus');
+const $buttonMinus = document.querySelector('.button_minus');
+const $bannerContent = Array.from(document.querySelectorAll('.banner_content'));
+const $bannerNumberBox = document.querySelector('.banner_number');
 
 let count = 0;
 $countBox.innerHTML = count;
+console.log($bannerContent.length); //liの数を取得
+$bannerNumberBox.innerHTML = $bannerContent.length;
 
 const timer = setInterval(() => {
-    if(count >= 5) {
-        clearInterval(timer);
-        count = 0;
-    } else {
-      count++;
-    }
-    $countBox.innerHTML = count;
+  count++;
+  if (count > 5) {
+      clearInterval(timer);
+      count = 0;
+  }
+  $countBox.innerHTML = count;
 }, 1000);
+
+$buttonPlus.addEventListener('click', () => {
+  count++;
+  $countBox.innerHTML = count;
+});
+
+$buttonMinus.addEventListener('click', () => {
+  count--;
+  $countBox.innerHTML = count;
+});
